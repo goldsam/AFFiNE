76c5cf3bddee1c198845541e7954404bb3bca6ce
Fangdun Tsai <fundon@pindash.io>
2025-03-11 23:14:31 +0800

    refactor(editor): edgeless frame toolbar config extension

--- blocksuite/affine/blocks/block-frame/src/frame-spec.ts
+++ blocksuite/affine/blocks/block-frame/src/frame-spec.ts
@@ -1,7 +1,10 @@
+import { FrameBlockSchema } from '@blocksuite/affine-model';
 import { BlockViewExtension } from '@blocksuite/block-std';
 import type { ExtensionType } from '@blocksuite/store';
 import { literal } from 'lit/static-html.js';
 
+const flavour = FrameBlockSchema.model.flavour;
+
 export const FrameBlockSpec: ExtensionType[] = [
-  BlockViewExtension('affine:frame', literal`affine-frame`),
+  BlockViewExtension(flavour, literal`affine-frame`),
 ];
